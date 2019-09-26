ffd21d90d406d17858b0199f5bbc34a2d141dd6d
daniel <alejo.dafr@gmail.com>
2019-09-25 20:22:50 -0500

    Added PSP0 directory

---
+++ src/ui/Main.java
@@ -0,0 +1,18 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
